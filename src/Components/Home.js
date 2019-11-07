b6a035fe551c762096cc246982eef5229b0adefb
dat1389 <datel1389@gmail.com>
2019-11-07 09:29:34 +0700

    up date home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import "./Login.css";
 export default class Home extends React.Component {
     render() {
         return (
             <div>
-                Homepage!
+                   <h1 id="hland">INVENTORY MANAGEMENT</h1>
+                 <p id="pland">easiest, fastest, most reliable</p>
+                 <button id="btnland" onclick="openWin()">Start managing</button>
             </div>
         )
     }
+    
+    
+    
+     openWin() {
+     window.open("http://localhost:3000/login", "_self", "width=500, height=500");
+    }
+    
+    
 }
