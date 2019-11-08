c612fa39a37f5921d1368bb7ca15010217c26aa9
dat1389 <datel1389@gmail.com>
2019-11-08 10:19:56 +0700

    function ko goi dk fix cho no chay di

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,16 +6,11 @@ export default class Home extends React.Component {
             <div>
                    <h1 id="hland">INVENTORY MANAGEMENT</h1>
                  <p id="pland">easiest, fastest, most reliable</p>
-                 <button id="btnland" onclick="openWin()">Start managing</button>
+                 <button id="btnland" onclick={this.openWin}>Start managing</button>
             </div>
         )
     }
     
-    
-    
-     openWin() {
-     window.open("http://localhost:3000/login", "_self", "width=500, height=500");
-    }
     
     
 }
